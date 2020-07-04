4774da645163083ac6257e8ea62460dc41b2d5d8
Андрей Рузаев <ruzaev-1999@mail.ru>
2020-07-04 09:18:00 +0300

    Добавьте файлы проекта.

---
+++ Variant19 №2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace v19_z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eng = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine("Введите предложение: ");
+            string s = Console.ReadLine();
+            string[] sentences = s.Split('!', '.', '?');
+            foreach (string sent in sentences)
+            {
+                string[] words = sent.Split('!', '.', '?', ';', ':', ',', ' ');
+
+                foreach (string word in words)
+                {
+
+                    int engCount = 0;
+
+                    for (int k = 0; k < word.Length; k++)
+                    {
+                        if (eng.Contains(word.ToLower()[k]))
+                        {
+                            engCount++;
+                        }
+                    }
+
+                    if (engCount != word.Length)
+                    {
+                        Console.Write($"{word} ");
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("пока");
+            Console.ReadLine();
+        }
+    }
+}
